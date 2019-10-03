15b8615813af10a45ee19fc24c8250d86d4df7ef
thomalu <thomalu@ssfs.org>
2019-10-03 10:04:25 -0400

    finished FightSong

---
+++ src/FightSong.java
@@ -0,0 +1,32 @@
+public class FightSong
+{
+    public static void goteam()
+    {
+        System.out.println("Go team, go!");
+        System.out.println("You can do it.");
+    }
+    public static void bestwest()
+    {
+        System.out.println("You're the best,");
+        System.out.println("In the west.");
+    }
+    public static void blankline()
+    {
+        System.out.println(" ");
+    }
+    public static void main(String [] args)
+    {
+        goteam();
+        blankline();
+        goteam();
+        bestwest();
+        goteam();
+        blankline();
+        goteam();
+        bestwest();
+        goteam();
+        blankline();
+        goteam();
+
+    }
+}
