7c138432164242384337495497727e204c2be572
thomalu <thomalu@ssfs.org>
2019-10-03 09:42:37 -0400

    finished FourRectanglePrinter

---
+++ src/FourRectanglePrinter.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+public class FourRectanglePrinter
+{
+    public static void main(String [] args)
+    {
+        int xCoordinate;
+        int yCoordinate;
+        int width = 5;
+        int height = 7;
+        Rectangle r = new Rectangle(0,0,width, height);
+        System.out.println(r);
+        r.translate(5,0);
+        System.out.println(r);
+        r.translate(0,7);
+        System.out.println(r);
+        r.translate(-5,0);
+        System.out.println(r);
+
+    }
+}
