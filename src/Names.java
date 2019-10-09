0e533df40c922c14c4fb8dffac48822f91948e09
thomalu <thomalu@ssfs.org>
2019-10-09 13:32:35 -0400

    finished names

--- src/Names.java
+++ src/Names.java
@@ -11,8 +11,22 @@ public class Names
         String last = scan.nextLine();
         int firstlength = first.length();
         int lastlength = last.length();
-        int firsts = first.substring(0,1)+last.substring(0,1);
+        String firsts = first.substring(0,1)+last.substring(0,1);
         int compare = first.compareTo(last);
+        String lastoffirst = first.substring(firstlength-1);
+        int index1 = last.indexOf(lastoffirst);
+        String lastoflast = last.substring(lastlength-1);
+        int index2 = first.indexOf(lastoflast);
+        String ffisrt =first.substring(0,firstlength/2)+last.substring(lastlength/2);
+        String llast = last.substring(0,lastlength/2)+first.substring(lastlength/2);
+
+        System.out.println(first+" "+last);
+        System.out.println(firstlength+lastlength);
+        System.out.println(firsts);
+        System.out.println(compare);
+        System.out.println(index1);
+        System.out.println(index2);
+        System.out.println(ffisrt+" "+llast);
 
     }
 }
