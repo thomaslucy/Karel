b7db0561487fbf2ce8c84f17dca9324611f5f676
thomalu <thomalu@ssfs.org>
2019-10-10 15:09:25 -0400

    working on wrappersdelight

---
+++ src/WrappersDelight.java
@@ -0,0 +1,13 @@
+public class WrappersDelight
+{
+    public static void main(String [] args)
+    {
+        int num1 = 14;
+        Integer num2 = new Integer(8);
+        double num3 = 2.5;
+        double num4 = num2+num3;
+        num3+=num1+
+
+
+    }
+}
